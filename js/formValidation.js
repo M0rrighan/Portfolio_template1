52cf915dd5d926521527bb6eb94a3938f00d0ec1
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-07-02 15:38:34 +0200

    Add Object to store data, Add function to populate the object, Add event listeners for form inputs

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,12 +1,49 @@
-const cForm = document.getElementById('form');
-const email = document.getElementById('email');
-// const alertMsg = document.querySelector('.contact_form .alert_msg');
+// Variables needed to check the email input validity
 const alertMsg = document.createElement('p');
 alertMsg.classList.add('alert_msg');
-// alertMsg.className('alert_msg');
 alertMsg.textContent = ' * Please change your email to lower case.';
 const submitBtn = document.querySelector('form .submit_btn button');
 
+// Variables to access form values
+const cForm = document.getElementById('form');
+const username = document.getElementById('name');
+const email = document.getElementById('email');
+const msg = document.getElementById('message');
+
+const formData = {
+  inputName: null,
+  inputEmail: null,
+  inputMessage: null,
+};
+
+// This function needs second parameter to be one of the following :
+// 'n' for name 'e' for email 'm' for message
+function populateFormData(value, itemName) {
+  if (itemName === 'n') {
+    formData.inputName = value;
+  } else if (itemName === 'e') {
+    formData.inputEmail = value;
+  } else if (itemName === 'm') {
+    formData.inputMessage = value;
+  } else {
+    console.log('Invalid attribute name for given value');
+  }
+}
+
+username.addEventListener('change', (event) => {
+  populateFormData(event.target.value, 'n');
+});
+email.addEventListener('change', (event) => {
+  populateFormData(event.target.value, 'e');
+});
+msg.addEventListener('change', (event) => {
+  populateFormData(event.target.value, 'm');
+});
+
+
+
+// Function to check if email input is lowercase
+// Alert if email input needs to be changed
 cForm.addEventListener('submit', (e) => {
   if (email.value !== email.value.toLowerCase()) {
     submitBtn.parentNode.insertBefore(alertMsg, submitBtn);
